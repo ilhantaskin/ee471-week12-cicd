bca375326057099b293879761ca74767d0c71fea
IlhanTaskin <ilhantaskin@std.iyte.edu.tr>
2026-06-04 14:03:40 +0300

    feat: add semantic versioning pipeline

---
+++ publish/bump_version.py
@@ -0,0 +1,12 @@
+import sys
+
+def bump_version(version):
+    with open("VERSION", "w") as f:
+        f.write(version)
+    print(f"Version bumped to {version}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python bump_version.py <version>")
+        sys.exit(1)
+    bump_version(sys.argv[1])
